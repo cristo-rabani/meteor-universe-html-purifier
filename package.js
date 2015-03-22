cd3cb5cba7f1ee7798f383f7b4e86d4877585672
cristo-rabani <cristo.rabani@gmail.com>
2015-03-22 10:09:24 +0100

    added git link

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
     summary: 'Package to cleaning up HTML from unknown/untrusted tags. It can help you in protection against XSS',
     name: 'vazco:universe-html-purifier',
-    version: '1.0.0'
+    version: '1.0.1',
+    git: 'https://github.com/cristo-rabani/meteor-universe-html-purifier.git'
 });
 
 Package.on_use(function (api) {
