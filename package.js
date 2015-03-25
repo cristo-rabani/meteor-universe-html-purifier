d42965f7edf24fb21f69827b37d269b4053b96e5
cristo-rabani <cristo.rabani@gmail.com>
2015-03-25 16:00:39 +0100

    small fix

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
     summary: 'Package to sanitize HTML from untrusted tags. Can help you in protection against XSS. + HTML5 parser',
     name: 'vazco:universe-html-purifier',
-    version: '1.0.3',
+    version: '1.1.0',
     git: 'https://github.com/cristo-rabani/meteor-universe-html-purifier.git'
 });
 
 Package.on_use(function (api) {
+    api.use([
+        'underscore@1.0.1'
+    ]);
     api.add_files([
         'HTMLParser.js',
         'HTMLPurifier.js'
